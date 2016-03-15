f94b7adef1601c6b1afad3676bbc88e731213fcd
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-03-16 00:39:35 +0700

    /public. add js file for theme free-starter

---
+++ public/js/contact_me.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function() {
+    $('#success').html('');
+});
---
+++ public/js/multiple_choice_test.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Hoang Anh on 25-Jan-16.
+ */
+var newDoc = document.implementation.createDocument(null, "html", null);
+console.log(newDoc);
